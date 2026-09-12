efdd6dc5701e0f8d71e990b79df0787a279d1805
Sumaiyah142112 <Airajunnisa@gmail.com>
2026-09-12 19:19:26 +0300

    add

---
+++ SQL/Data_Science.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+arr = np.array([1,2,3,4,7])
+
+print(arr)
+
+print(arr[3])
+print(arr[-2])
+
+print(arr[2:4])
+
+for i in arr:
+    print(i)
+
+for i in range(0,len(arr)):
+    print(arr[i])
+
+
+
